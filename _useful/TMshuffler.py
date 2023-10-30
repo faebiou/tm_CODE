60638a924bdbcfa3d310656550939a81191ee1b3
Fabio Pop <62741006+faebiou@users.noreply.github.com>
2023-10-30 12:45:15 +0100

    TMshuffler edit

---
+++ _useful/TMshuffler.py
@@ -0,0 +1,16 @@
+size("A4")
+
+TM = ["Felix", "Ando", "Nina", "Fabio", "Ben", "Betsy", "Max", "Zhenya", "Mihael", "Mint", "Pauline", "Ceci"]
+
+shuffle(TM)
+
+fs = 32
+lh = 2
+font("Menlo")
+fontSize(fs)
+
+for student in range(len(TM)):
+    text(TM[student] + " → " + TM[student-1], (64, 60))
+    translate(0, fs*lh)
+    
+saveImage("../_exp/TMshuffler.pdf")
